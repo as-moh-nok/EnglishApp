14d21231ff80c4286017c44b9bdf90db582a0a37
as-moh-nok <a.mohamma.nok@gmail.com>
2023-03-10 17:06:58 +0330

    add Database

---
+++ DB.js
@@ -0,0 +1,34 @@
+const que_ans = [ {"que": "What time shall we go shopping? Let’s see how the weather looks and .... it .... .... .",
+                   "ans": [ "play it by ear"],
+                   "half_ans" : " .... it .... .... .",
+                   "buttons": [ "game","with","by", "nose",
+                       "ear" ,"hear","show" ,"play"]
+                },
+                {"que": "In fact he’s not right for this role, he has his .... in .... .... .",
+                 "ans": "head in the cloud",
+                 "half_ans" : " .... in .... ..... ",
+                 "buttons": ["face" ,"the" ,"mind" , "sky"  ,"top","head" , "star" ,"cloud"]
+            },
+            {"que": "What time shall we go shopping? Let’s see how the weather looks and .... it .... .... .",
+                   "ans": [ "play it by ear"],
+                   "half_ans" : " .... it .... .... .",
+                   "buttons": [ "Game","with","by", "nose",
+                       "ear" ,"hear","show" ,"play"]
+                },
+                {"que": "In fact he’s not right for this role, he has his .... in .... .... .",
+                 "ans": "Head in the cloud",
+                 "half_ans" : " .... in .... ..... ",
+                 "buttons": ["face" ,"the" ,"mind" , "sky"  ,"top","head" , "star" ,"cloud"]
+            },
+                {"que": "What time shall we go shopping? Let’s see how the weather looks and .... it .... .... .",
+                "ans": [ "play it by ear"],
+                "half_ans" : " .... it .... .... .",
+                "buttons": [ "game","with","by", "nose",
+                    "ear" ,"hear","show" ,"play"]
+             },
+             {"que": "In fact he’s not right for this role, he has his .... in .... .... .",
+             "ans": "head in the cloud",
+             "half_ans" : " .... in .... .... ",
+             "buttons": ["face" ,"the" ,"mind" , "sky"  ,"top","head" , "star" ,"cloud"]
+        }
+]
