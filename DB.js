c19fb1385331c0f5692939cd67ac14a52954b84b
as-moh-nok <a.mohamma.nok@gmail.com>
2023-03-11 13:19:31 +0330

    add Next btn

--- DB.js
+++ DB.js
@@ -1,23 +1,26 @@
-const que_ans = [ {"que": "What time shall we go shopping? Let’s see how the weather looks and .... it .... .... .",
+const que_ans = [ {"que": "what time shall we go shopping? Let’s see how the weather looks and ---- it ----   ---- .",
                    "ans": [ "play it by ear"],
-                   "half_ans" : " .... it .... .... .",
-                   "buttons": [ "game","with","by", "nose",
-                       "ear" ,"hear","show" ,"play"]
+                   "half_ans" : " ---- it ---- ----",
+                   "buttons": [ "game","with","by", "nose", "ear" ,"hear","show" ,"play"]
+                    ,
+                    "index" : 14
                 },
-                {"que": "In fact he’s not right for this role, he has his .... in .... .... .",
+                {"que": "in fact he’s not right for this role, he has his ---- in ---- ----.",
                  "ans": "head in the cloud",
-                 "half_ans" : " .... in .... ..... ",
-                 "buttons": ["face" ,"the" ,"mind" , "sky"  ,"top","head" , "star" ,"cloud"]
+                 "half_ans" : "---- in ---- ----",
+                 "buttons": ["face" ,"the" ,"mind" , "sky"  ,"top","head" , "star" ,"cloud"],
+                 "index" : 12
             },
-            {"que": "What time shall we go shopping? Let’s see how the weather looks and .... it .... .... .",
-                   "ans": [ "play it by ear"],
-                   "half_ans" : " .... it .... .... .",
-                   "buttons": [ "Game","with","by", "nose",
-                       "ear" ,"hear","show" ,"play"]
-                },
-                {"que": "In fact he’s not right for this role, he has his .... in .... .... .",
-                 "ans": "Head in the cloud",
-                 "half_ans" : " .... in .... ..... ",
+            {"que": "what time shall we go shopping? Let’s see how the weather looks and ---- it ---- ---- .",
+            "ans": [ "play it by ear"],
+            "half_ans" : " ---- it ---- ----",
+            "buttons": [ "game","with","by", "nose", "ear" ,"hear","show" ,"play"]
+             ,
+             "index" : 14
+         },
+                {"que": "teacher’s colourful waistcoats and unusual taste for hats made him a ---- -than- ---- character in the local community.",
+                 "ans": "larger than life",
+                 "half_ans" : " ---- -than- ----  ",
                  "buttons": ["face" ,"the" ,"mind" , "sky"  ,"top","head" , "star" ,"cloud"]
             },
                 {"que": "What time shall we go shopping? Let’s see how the weather looks and .... it .... .... .",
