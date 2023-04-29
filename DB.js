a9c243e7a08760ff9a95873f34c36b82dca9c96a
as-moh <a.mohamma.nok@gmail.com>
2023-04-29 18:53:39 +0430

    fix some bugs

--- DB.js
+++ DB.js
@@ -2,8 +2,8 @@
 const que_ans = [ //question 1
 {"que": "what time shall we go shopping? Let’s see how the weather looks and ---- it ----   ---- .",
 "ans":  "play it by ear",
-    	"half_ans" : "---- it ---- ----",
-    	"buttons": [ "game","with","by", "nose", "ear" ,"hear","show" ,"play"] ,
+        "half_ans" : "---- it ---- ----",
+        "buttons": [ "game","with","by", "nose", "ear" ,"hear","show" ,"play"] ,
 "index" : 14,
 "image" : "img/idiom1.png"
       },
@@ -15,14 +15,14 @@ const que_ans = [ //question 1
 "index" : 12,
 "image" : "img/idiom2.png"
        },
-{"que":" teacher’s colourful waistcoats and unusual taste for hats made him a ---- -than- ---- character in the local community.",
+{"que":"teacher’s colourful waistcoats and unusual taste for hats made him a ---- -than- ---- character in the local community.",
  "ans": "larger than life",
  "half_ans" : "---- than ----",
  "buttons": [ "bigger","life","smaller", "larger", "little" ,"living", "spirit" ,"love"],
 "index" : 12,
 "image" : "img/idiom3.png"
          },
-         {"que":" I thought it was about time I went over there and ---- ---- ---- branch.",
+         {"que":"I thought it was about time I went over there and ---- ---- ---- branch.",
          "ans": "extended the olive branch",
          "half_ans" : "---- ---- ---- branch",
          "buttons": [ "garlic","a","expanded", "onion", "the" ,"developed","extended" ,"olive"],
@@ -37,97 +37,107 @@ const que_ans = [ //question 1
                 "image" : "img/idiom5.png"
                          },
                     
-{"que":" without meaning to ---- my own -----, I came top of the class and no one noticed it before.",
-"ans": " blow my own trumpet",
-"half_ans" : "---- my own -----",
-"buttons": [ "tantara","puff","blow", "the", "reed" ,"play","piano" ,"trumpet"],
+{"que":"without meaning to ---- my own -----, I came top of the class and no one noticed it before.",
+"ans":"blow my own trumpet",
+"half_ans": "---- my own ----",
+"buttons": [ "tantara","puff","blow","the","reed","play","piano","trumpet"],
 "index" :4 ,
 "image" : "img/idiom6.png"
         },
-{"que":" after vehemently arguing, he discovered his mistake and had to eat ---- ---- before his associates.",
+{"que":"after vehemently arguing, he discovered his mistake and had to eat ---- ---- before his associates.",
 "ans": "eat humble pie",
 "half_ans" : "eat ---- ----",
-"buttons": [ "cake","cookie","pie", "the", "chocolate" ,"humble","blushing" ,"candy"],
+"buttons": [ "cake","cookie","pie","the","chocolate","humble","blushing" ,"candy"],
 "index" :16 ,
 "image" : "img/idiom7.png"
 
         },
-{"que":" the workers were promised that they would not lose their jobs because of the merger. Later they found out that they had been ---- down ---- ---- .",
-"ans": " sold down the river",
-"half_ans" : "---- down ---- ---- ",
-"buttons": [ "stuck","sea","river", "the", "sold" ,"road","smoke" ,"ski"],
+{"que":"the workers were promised that they would not lose their jobs because of the merger. Later they found out that they had been ---- down ---- ----.",
+"ans": "sold down the river",
+"half_ans": "---- down ---- ----",
+"buttons": [ "stuck","sea","river","the","sold","road","smoke","ski"],
 "index" :24,
-"image" : "img/idiom8.png"
+"image": "img/idiom8.png"
         },
-        {"que":" my daughter feels very bad and is easily upset. If anything goes wrong, she will ---- off ---- ---- .",
-        "ans": " fly off the handle",
-        "half_ans" : "---- off ---- ----",
-        "buttons": [ "hand","flow","the", "fly", "rattle" ,"bottle","flab" ,"handdle"],
+        {"que":"My daughter feels very bad and is easily upset. If anything goes wrong, she will ---- off ---- ----.",
+        "ans": "fly off the handle",
+        "half_ans": "---- off ---- ----",
+        "buttons": [ "hand","flow","the", "fly", "rattle","bottle","flab","handle"],
        "index" :16,
-       "image" : "img/idiom9.png"
+       "image": "img/idiom9.png"
                 },
-       {"que":" he’s fresh out of dental school and trying to set up a practise. But he does good work, and he won’t ----- you an ---- ---- a ---- for it like most other dentists.",
-        "ans": " cost you an arm and a leg  ",
-        "half_ans" : "---- you an ---- ---- a ----  ",
-        "buttons": [ "catch","and","price", "cost", "ear" ,"leg","cast" ,"arm"],
+       {"que":"He’s fresh out of dental school and trying to set up a practise. But he does good work, and he won’t ----- you an ---- ---- a ---- for it like most other dentists.",
+        "ans": "cost you an arm and a leg",
+        "half_ans": "---- you an ---- ---- a ----",
+        "buttons": [ "catch","and","price", "cost", "ear","leg","cast" ,"arm"],
        "index" :22,
-       "image" : "img/idiom10.png"
+       "image": "img/idiom10.png"
                 },
-       {"que":" your lost? don’t worry just ask Jenny.she ---- the ---- very well.",
+       {"que":"Your lost? don’t worry just ask Jenny. She ---- the ---- very well.",
         "ans": "knows the rope",
-        "half_ans" : "---- the ---- ",
-        "buttons": [ "tight","ripe","study", "knows", "understand" ,"rope","needle" ,"ride"],
+        "half_ans": "---- the ---- ",
+        "buttons": [ "tight","ripe","study", "knows", "understand","rope","needle","ride"],
        "index" :9,
-       "image" : "img/idiom11.png"
+       "image": "img/idiom11.png"
                 },
-       {"que":" conflicting countries should work on the way to ---- the ---- in order to avoid going to war.",
+       {"que":"Conflicting countries should work on the way to ---- the ---- in order to avoid going to war.",
         "ans": "bury the hatchet",
-        "half_ans" : "---- the ----",
-        "buttons": [ "bite","carry","hat", "catch", "beat" ,"burry","cast" ,"hatchet"],
-       "index" :9
+        "half_ans": "---- the ----",
+        "buttons": [ "bite","carry","hat", "catch", "beat","bury","cast","hatchet"],
+       "index" :9,
+       "image": "img/idiom12.png"
                 },
-                {"que":" I hate my new job, it's so stressful and I can't wait until the weekends when I go home and ---- ---- ----  down with my friends.",
-                "ans": " let my hair down",
-               "half_ans" : "---- ---- ---- down ",
-                "buttons": [ "late","head","the", "your", "hair" ,"neck","let" ,"look"],
-               "index" :21
+                {"que":"I hate my new job, it's so stressful and I can't wait until the weekends when I go home and ---- ---- ---- down with my friends.",
+                "ans": "let my hair down",
+               "half_ans": "---- ---- ---- down ",
+                "buttons": [ "late","head","the", "my", "hair","neck","let" ,"look"],
+               "index" :21,
+               "image": "img/idiom13.png"
                         },
-               {"que":" I can’t stand July anymore. she always ---- me ---- the ---- way in the office.",
-                "ans": " rub me up the   wrong way",
-                "half_ans" : " ---- me ---- the ---- way  ",
-                "buttons": [ "tap","up","ring", "right", "down" ,"rub","into" ,"wrong","wipe"],
-               "index" :8
+               {"que":"I can’t stand July anymore. she always ---- me ---- the ---- way in the office.",
+                "ans": "rub me up the wrong way",
+                "half_ans" : "---- me ---- the ---- way",
+                "buttons": [ "tap","up","ring", "right", "down","rub","into" ,"wrong","wipe"],
+               "index" :8,
+               "image": "img/idiom14.png"
                         },
-               {"que":" everyone has to ---- the ---- and face their fears in their lives.",
-                "ans": " bite the bullet",
-               "half_ans" : "---- the ---- ",
-                "buttons": [ "beat","battle","ball", "bullet", "ladder" ,"bite","leader" ,"blood"],
-               "index" :4
+               {"que":"Everyone has to ---- the ---- and face their fears in their lives.",
+                "ans": "bite the bullet",
+               "half_ans": "---- the ----",
+                "buttons": [ "beat","battle","ball", "bullet", "ladder","bite","leader","blood"],
+               "index" :4,
+               "image": "img/idiom15.png"
                         },
-               {"que":" if you were still living at home, you knew the experience of being ---- the ---- act by parents when these calls started appearing on the phone bill.",
-                "ans": " read the riot act",
-               "half_ans" : "---- the ---- act ",
-                "buttons": [ "write","reed","beat", "read", "bit" ,"bite","riot" ,"react"],
-               "index" :14
+               {"que":"If you were still living at home, you knew the experience of being ---- the ---- act by parents when these calls started appearing on the phone bill.",
+                "ans": "read the riot act",
+               "half_ans": "---- the ---- act",
+                "buttons": [ "write","reed","beat", "read", "bit","bite","riot","react"],
+               "index" :14,
+               "image": "img/idiom16.png"
                         },
-                        {"que":" When I asked Alex whether he knew who had taken the files from my desk, he started ---- around ---- ---- and refused to give me a direct answer.",
-                        "ans": " beat around the bush ",
-                       "half_ans" : "---- around ---- ---- ",
-                        "buttons": [ "bite","bitter","a", "the", "boss" ,"bite","bat" ,"bush"],
-                       "index" :18
+                        {"que":"When I asked Alex whether he knew who had taken the files from my desk, he started ---- around ---- ---- and refused to give me a direct answer.",
+                        "ans": "beat around the bush",
+                       "half_ans" : "---- around ---- ----",
+                        "buttons": [ "beat","bitter","a", "the", "boss" ,"bite","bat" ,"bush"],
+                       "index" :18,
+                       "image": "img/idiom17.png"
                                 },
-                       {"que":" if you don't believe me, go to his room and talk to him to hear it ---- from ---- ----’s ---- .",
-                        "ans": " straight from the horse’s mouth  ",
-                       "half_ans" : "---- from ----‘s ---- ",
+                       {"que":"If you don't believe me, go to his room and talk to him to hear it ---- from ---- ----’s ---- .",
+                        "ans": "straight from the horse’s mouth",
+                       "half_ans" : "---- from ---- ‘s ----",
                         "buttons": [ "through","house","straight", "sheep", "horse" ,"the","mouth" ,"lips"],
-                       "index" :17
+                       "index" :17,
+                       "image": "img/idiom18.png"
                                 },
-                       {"que":" it's best to always take what he says with a ----- of ---- – what he just does is to exaggerate everything.",
-                        "ans": " pinch of salt ",
+                       {"que":"It's best to always take what he says with a ----- of ---- what he just does is to exaggerate everything.",
+                        "ans": "pinch of salt",
                        "half_ans" : "---- of ----",
-                        "buttons": [ "went","stuck","got", "gave", "stick" ,"gate","sack" ,"gut"],
-                       "index" :11
+                        "buttons": [ "salt","stuck","pinch", "gave", "stick" ,"gate","sack" ,"gut"],
+                       "index" :11,
+                       "image": "img/idiom19.png"
                                 },
                        
                             ]          
        
+
+
